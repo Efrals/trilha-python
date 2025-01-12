b7ae555363b4231db6b7161fa3a77095e8c23029
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2025-01-11 22:14:17 -0300

    strings

---
+++ 06strings/fatiamento.py
@@ -0,0 +1,22 @@
+nome = "Efraim Rocha da Silva"
+
+#conta o primeiro como 0
+print (nome [0])
+
+#retornará caracteres em ordem inversa do que foi passado
+print (nome [-2])
+
+#não conta o primeiro como 0
+print (nome [:6])
+
+#não ignora espaços
+print (nome [6:])
+
+#limita o início e o fim
+print (nome [6:19])
+
+            #início, fim, passo
+print (nome [6:19:2])
+
+#espelha os caracteres
+print (nome [::-1])
---
+++ 06strings/stringstriplas.py
@@ -0,0 +1,18 @@
+#mantém o tudo o que foi passado na string
+
+amostra = "Significado de texto"
+
+texto = f"""
+            {amostra}
+  1.
+    conjunto das palavras escritas, em livro, folheto, documento etc.
+    (p.opos. a comentários, aditamentos, sumário etc.);
+    redação original de qualquer obra escrita.
+    "um t. manuscrito"
+
+  2.
+    trecho ou fragmento de obra de um autor.
+    "o t. de Graciliano Ramos"
+"""
+
+print (texto)
