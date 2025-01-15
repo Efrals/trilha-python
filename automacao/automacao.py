a1effcab1fdfa36264dec576cf33596691bbbb11
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2025-01-15 11:54:37 -0300

    projetinhos

--- automacao/automacao.py
+++ automacao/automacao.py
@@ -51,30 +51,37 @@ print(tabela)
 
 # Passo 4: Cadastrar um produto
 for linha in tabela.index:
-    # clicar no campo de código
+    # clicar no 1º campo
     pyautogui.click(x=653, y=294)
+
     # pegar da tabela o valor do campo que a gente quer preencher
     codigo = tabela.loc[linha, "codigo"]
-    # preencher o campo
-    pyautogui.write(str(codigo))
-    # passar para o proximo campo
+    pyautogui.write(str(codigo))    # preencher o campo
     pyautogui.press("tab")
-    # preencher o campo
-    pyautogui.write(str(tabela.loc[linha, "marca"]))
+
+
+    pyautogui.write(str(tabela.loc[linha, "marca"]))    # preencher o campo
     pyautogui.press("tab")
+
     pyautogui.write(str(tabela.loc[linha, "tipo"]))
     pyautogui.press("tab")
+
     pyautogui.write(str(tabela.loc[linha, "categoria"]))
     pyautogui.press("tab")
+
     pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
     pyautogui.press("tab")
+
     pyautogui.write(str(tabela.loc[linha, "custo"]))
     pyautogui.press("tab")
+
     obs = tabela.loc[linha, "obs"]
     if not pd.isna(obs):
         pyautogui.write(str(tabela.loc[linha, "obs"]))
     pyautogui.press("tab")
-    pyautogui.press("enter") # cadastra o produto (botao enviar)
-    # dar scroll de tudo pra cima, negativo d[a scroll para baixo]
+
+    pyautogui.press("enter")    # cadastra o produto (botao enviar)
+
+    # dar scroll de tudo pra cima, negativo para dar scroll para baixo
     pyautogui.scroll(5000)
     # Passo 5: Repetir o processo de cadastro até o fim
