1f727b61914da8c9215d2b94b84ba62bf36e077b
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2025-02-14 22:21:33 -0300

    tuplas

---
+++ 08tuplas/tuplas.py
@@ -0,0 +1,38 @@
+#
+#Tuplas são estrutura imutáveis
+#
+#Tuplas usam parênteses         #Boa prática finalizar com vírgula no último item
+moveis = ("cama", "mesa", "sofa",)
+
+#Declara explicitamente que se trata de uma tupla
+objeto = tuple("xícara")
+numeros = tuple([1, 2, 3])
+
+#Python pode se confundir ao declarar somente 1 item na tupla sem especificar explicitamente que se trata de uma tupla
+                                   #Boa prática finalizar com vírgula no último item
+palavra = ("otorrinolaringologista",)
+
+matriz = (
+    (0, 1, 2, 3),
+    ("a", "b", "c"),
+    ("0a", "1b", "2c"),
+)
+
+print(moveis[-1])
+print(objeto[1])
+print(numeros[2])
+print(palavra[0])
+
+
+#Sempre retornará exceção ao tentar fazer qualquer alteração nas tuplas
+matriz.clear()
+print(matriz)
+
+palavra.extend(["outra"])
+print(palavra)
+
+moveis.pop()
+print(moveis)
+
+objeto.remove("xícara")
+print(objeto)
